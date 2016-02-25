bfc848679e3ca449f750193d1cb2c3ea7f77699a
Sven König <sven@lokaler.de>
2016-02-25 15:18:27 +0100

    Questionnaire displays data

--- src/components/Questionnaire/Questionnaire.js
+++ src/components/Questionnaire/Questionnaire.js
@@ -8,10 +8,32 @@ export default class Questionnaire extends React.Component {
     appState: PropTypes.object.isRequired
   };
 
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+    try {
+      const response = await fetch('/data/questionnaire.json');
+      const data = await response.json();
+      this.setState({ data });
+    } catch (e) {
+      throw e;
+    }
+  }
+
   render() {
+    const data = JSON.stringify(this.state.data);
     return (
       <div>
         <h3>Questionnaire</h3>
+        <code>
+          { data }
+        </code>
       </div>
     );
   }
