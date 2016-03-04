190ce78b840dc011386931318233a030cfded8d6
moklick <info@moritzklack.com>
2016-03-04 11:43:50 +0100

    fix store to use redux extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,21 +2,16 @@
 // This boilerplate file is likely to be the same for each project that uses Redux.
 // With Redux, the actual stores are in /reducers.
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
-  let store;
-  const thunkMiddleWare = applyMiddleware(thunk);
-  // Enable Redux devtools if the extension is installed in developer's browser
-  // @TODO:window.devToolsExtension()(createStore)(rootReducer, thunkMiddleWare, initialState)
-  // is not working. The middleware is not attached to the store. I don't know why..
-  if (false && window.devToolsExtension) {
-    store = window.devToolsExtension()(createStore)(rootReducer, thunkMiddleWare, initialState);
-  } else {
-    store = createStore(rootReducer, thunkMiddleWare, initialState);
-  }
+  const store = createStore(rootReducer, initialState, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
