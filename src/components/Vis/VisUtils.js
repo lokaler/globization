a9a80054df75120dc1b5d427a847d3d0525e4f8e
Christopher Pietsch <cpietsch@gmail.com>
2016-03-07 17:02:47 +0100

    debugging off

--- src/components/Vis/VisUtils.js
+++ src/components/Vis/VisUtils.js
@@ -1,9 +1,12 @@
+/* eslint-disable */
+
 export default {
   calculateBla: (params) => {
     const newValue = 123 + params;
     return newValue;
   },
   log: (...theArgs) => {
-    if (false) console.log.apply(console, theArgs);
+    const debug = true;
+    if (debug) console.log.apply(console, theArgs);
   }
 };
