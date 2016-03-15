f77ac7767390f3210ba21d8f2bde2c744726b10a
Christopher Pietsch <cpietsch@gmail.com>
2016-03-15 22:17:44 +0100

    final count down

---
+++ dist/data/texts.js
@@ -0,0 +1,37 @@
+export const intro = {
+
+  de: `
+    ÜberMorgen-Explorer
+    ===================
+
+    Vergleichen Sie Ihr Konsumverhalten mit dem von Menschen in anderen Ländern.
+    Und finden Sie heraus, wie Sie Umwelt und Ressourcen schonen können.
+
+    <div class="losButton" data-onclick="onStartClick">Los geht’s</div>
+
+    Oder erkunden Sie die Datensätze:
+  `,
+
+  en: `
+    BeyondTomorrow Explorer
+    ===================
+
+    Compare your consumption habits with those of people from other countries --
+    and find out how you can protect the environment and conserve natural resources.
+
+    <div class="losButton" data-onclick="onStartClick">Let's go</div>
+
+    Or go ahead explore the data sets.
+  `
+
+};
+
+export const pagination = {
+  prev: { de: 'ZURÜCK', en: 'PREV' },
+  next: { de: 'WEITER', en: 'NEXT' }
+};
+
+export const vis = {
+  source: { de: 'Quelle', en: 'Source' },
+  globe: { de: 'Globus', end: 'Gobe' }
+};
