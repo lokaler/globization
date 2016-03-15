b787a89f8786f9eb56902c8166b8088d17b918f0
Sven König <sven@lokaler.de>
2016-03-15 18:36:34 +0100

    q: added translation for Intro

--- src/components/Questionnaire/Intro.js
+++ src/components/Questionnaire/Intro.js
@@ -1,28 +1,38 @@
 import React, { PropTypes } from 'react';
 import cssModules from 'react-css-modules';
 import DataSetList from '../Ubermorgen/DataSetList';
+import TranslatedMarkdown from './TranslatedMarkdown';
+import * as texts from 'data/texts';
+
 
 @cssModules()
-export default class Outro extends React.Component {
+export default class Intro extends React.Component {
 
   static propTypes = {
     questions: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired,
     master: PropTypes.object.isRequired
   }
-  onStartClick() {
-    this.props.actions.setCard(0, null);
-  }
-  render() {
-    const boundClickHandler = this.onStartClick.bind(this);
 
-    return (<div>
-      <h1>ÜberMorgen-Explorer</h1>
-      <p>Vergleichen Sie Ihr Konsumverhalten mit dem von Menschen in anderen Ländern.
-      Und finden Sie heraus, wie Sie Umwelt und Ressourcen schonen können.</p>
-      <div className="losButton" onClick={ boundClickHandler }>Los geht’s</div>
-      <p>Oder erkunden Sie die Datensätze auf eigene Faust:</p>
-      <DataSetList { ...this.props } />
-    </div>);
+  onClick = (evt) => {
+    const handler = this.clickHandlers[evt.target.dataset.onclick];
+    if (handler) {
+      handler(evt);
+    }
+  }
+
+  clickHandlers = {
+    onStartClick: () => {
+      this.props.actions.setCard(0, null);
+    }
+  }
+
+  render() {
+    return (
+      <div onClick={ this.onClick }>
+        <TranslatedMarkdown text={ texts.intro }/>
+        <DataSetList { ...this.props } />
+      </div>
+    );
   }
 }
---
+++ src/data/texts.js
@@ -0,0 +1,27 @@
+export const intro = {
+
+  de: `
+    ÜberMorgen-Explorer
+    ===================
+
+    Vergleichen Sie Ihr Konsumverhalten mit dem von Menschen in anderen Ländern.
+    Und finden Sie heraus, wie Sie Umwelt und Ressourcen schonen können.
+
+    <div class="losButton" data-onclick="onStartClick">Los geht’s</div>
+
+    Oder erkunden Sie die Datensätze auf eigene Faust
+  `,
+
+  en: `
+    ÜberMorgen-Explorer (*** ENGLISH ***)
+    ===================
+
+    Vergleichen Sie Ihr Konsumverhalten mit dem von Menschen in anderen Ländern.
+    Und finden Sie heraus, wie Sie Umwelt und Ressourcen schonen können.
+
+    <div class="losButton" data-onclick="onStartClick">Los geht’s</div>
+
+    Oder erkunden Sie die Datensätze auf eigene Faust
+  `
+
+};
