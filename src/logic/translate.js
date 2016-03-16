301c5ff308d748bdc64cb0c51c539a5e4c8505c1
Sven König <sven@lokaler.de>
2016-03-16 16:31:49 +0100

    app: translate() translates country codes

--- src/logic/translate.js
+++ src/logic/translate.js
@@ -1,13 +1,37 @@
 import store from '../store';
-import { isUndefined, isObject, isString } from 'lodash';
+import { isUndefined, isObject, isString, get } from 'lodash';
 import translations from 'data/translations.json';
+import master from 'data/master.csv';
 
-export default function translate(value) {
+const countries = {};
+for (const c of master) {
+  const isoCode = c.alpha3;
+  countries[isoCode] = {
+    en: {
+      default: c.name,
+      locative: c.locaEN
+    },
+    de: {
+      default: c.name_deu,
+      locative: c.loca
+    }
+  };
+}
+
+export default function translate(value, options = {}) {
   const language = store.getState().app.language;
 
   // multilanguage text object
   if (isObject(value) && 'de' in value && 'en' in value) {
     return value[language];
+  }
+
+  // simple string that is a country iso code
+  if (options.isCountryCode) {
+    const case_ = options.case || 'default';
+    const path = `${value }.${ language }.${ case_ }`;
+    console.log('parh', path);
+    return get(countries, path);
   }
 
   // simple string
