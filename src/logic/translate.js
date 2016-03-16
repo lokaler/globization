701d6c89eb6b9fe4df08c87bb896c98f643ae882
Sven König <sven@lokaler.de>
2016-03-16 13:01:59 +0100

    app: translate support simple strings via translations.json

--- src/logic/translate.js
+++ src/logic/translate.js
@@ -1,11 +1,27 @@
 import store from '../store';
-import { isObject } from 'lodash';
+import { isUndefined, isObject, isString } from 'lodash';
+import translations from 'data/translations.json';
 
 export default function translate(value) {
   const language = store.getState().app.language;
+
   // multilanguage text object
   if (isObject(value) && 'de' in value && 'en' in value) {
     return value[language];
+  }
+
+  // simple string
+  if (isString(value)) {
+    const translated = ((translations[value] || {})[language]);
+    if (isUndefined(translated)) {
+      if (__DEV__) {
+        console.warn(
+          `Translation for "${ value }" (${ language }) is missing.`
+        ); // eslint-disable-line no-console
+      }
+      return value;
+    }
+    return translated;
   }
   return value;
 }
