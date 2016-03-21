66e30616126e0d58ed63821f381f37c379841e5c
Sven König <sven@lokaler.de>
2016-03-21 15:57:30 +0100

    app: cleaned up AppReducer

--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -1,8 +1,8 @@
-import * as ActionTypes from '../constants/ActionTypes';
+import { GET_URL_PARAMETERS, SET_WINDOW_SIZE, SPON_LOGGER } from '../constants/ActionTypes';
 import { includes } from 'lodash';
 import d3 from 'd3';
 const bbox = d3.select('body').node().getBoundingClientRect();
-const width = bbox.width > 860 ? 860 : bbox.width;
+const width = Math.min(bbox.width, 860);
 
 const initialState = {
   language: 'de',
@@ -24,16 +24,16 @@ function getQueryVariable(variable) {
 
 export default function appReducer(state = initialState, action) {
   switch (action.type) {
-    case ActionTypes.GET_URL_PARAMETERS: {
+    case GET_URL_PARAMETERS: {
       let language = getQueryVariable('language') || 'de';
       if (!includes(['de', 'en'], language)) language = 'de';
       const mobile = !!parseInt(getQueryVariable('mobile') || '0', 10);
-      return Object.assign({}, state, { language, mobile });
+      return { ...state, language, mobile };
     }
-    case ActionTypes.SET_WINDOW_SIZE: {
-      return Object.assign({}, state, { ...action.sizes });
+    case SET_WINDOW_SIZE: {
+      return { ...state, ...action.sizes };
     }
-    case ActionTypes.SPON_LOGGER: {
+    case SPON_LOGGER: {
       try {
         const sponFrame = window.parent.$ || false;
         // console.log('logger', sponFrame);
