c0d9c23b8e6ac849bfbb66734696c049a203fdbf
Lorenz Matzat <matzat@lokaler.de>
2016-04-11 16:02:17 +0200

    added all cards for 4/16

--- src/data/questionnaires/0416/index.js
+++ src/data/questionnaires/0416/index.js
@@ -1,4 +1,9 @@
 export default [
   require('./card-01-foo.json'),
-  require('./card-02-foo.json')
+  require('./card-02-foo.json'),
+  require('./card-03-kids.json'),
+  require('./card-04-diabetes.json'),
+  require('./card-05-sports.json'),
+  require('./card-06-alcohol.json'),
+  require('./card-07-summary.json')
 ];
