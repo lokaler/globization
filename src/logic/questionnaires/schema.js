3c05eb406ffd1ed713d1f335003c9a2e246eba9f
Sven König <sven@lokaler.de>
2016-04-12 15:03:00 +0200

    logic: improved validation

--- src/logic/questionnaires/schema.js
+++ src/logic/questionnaires/schema.js
@@ -10,115 +10,92 @@ function array(schema = {}) {
   return Object.assign({ type: 'array' }, schema);
 }
 
-function ref(definition) {
+function contentPart(key, schema = {}) {
   return {
-    $ref: `#/definitions/${ definition }`
-  };
-}
-
-function contentPart(key, schema) {
-  return {
+    $schema: 'http://json-schema.org/draft-04/schema#',
     required: [key],
     additionalProperties: false,
-    properties: {
-      [key]: schema
-    }
+    properties: { [key]: schema }
   };
 }
 
-export default {
+export const questionnaire = array({
 
   $schema: 'http://json-schema.org/draft-04/schema#',
 
-  definitions: {
-
-    arrayOfStrings: array({ items: string() }),
-
-    multiLangArrayOfStrings: object({
-      required: ['de', 'en'],
-      additionalProperties: true,
-      properties: {
-        action: string(),
-        de: ref('arrayOfStrings'),
-        en: ref('arrayOfStrings')
-      }
-    }),
-
-    text: contentPart(
-      'text',
-      ref('multiLangArrayOfStrings')
-    ),
-
-    input: contentPart(
-      'input',
-      object({
-        required: ['key', 'type', 'options'],
-        properties: {
-          key: string(),
-          type: { enum: ['slider', 'choices'] },
-          options: { type: 'object' }
-        }
-      })
-    ),
-
-    answer: contentPart(
-      'answer',
-      object({
-        required: ['templates'],
-        additionalProperties: false,
-        properties: {
-          className: { enum: ['tip'] },
-          answerKey: array(),
-          answerContext: object({
-            patternProperties: { '.*': string() }
-          }),
-          templates: {
-            patternProperties: { '.*': ref('multiLangArrayOfStrings') }
-          }
-        }
-      })
-    ),
-
-    answerQuiz: contentPart(
-      'answerQuiz',
-      object({
-        required: ['templates', 'key'],
-        additionalProperties: false,
-        properties: {
-          key: string(),
-          actions: array(),
-          className: { enum: ['tip'] },
-          answerKey: array(),
-          answerContext: object({
-            patternProperties: { '.*': string() }
-          }),
-          templates: {
-            patternProperties: { '.*': ref('multiLangArrayOfStrings') }
-          }
-        }
-      })
-    ),
-
-    card: array({
-      items: object({
-        oneOf: [
-          ref('text'),
-          ref('input'),
-          ref('answer'),
-          ref('answerQuiz')
-        ]
-      })
-    })
-  },
-
-  type: 'array',
   items: object({
     required: ['title', 'content'],
     properties: {
       title: string(),
       dataset: string(),
-      functions: ref('arrayOfStrings'),
-      content: ref('card')
+      content: array({
+        items: object({
+          oneOf: [
+            contentPart('text'),
+            contentPart('input'),
+            contentPart('answer'),
+            contentPart('answerQuiz')
+          ]
+        })
+      })
     }
   })
+});
+
+const arrayOfStrings = array({ items: string() });
+const multiLangArrayOfStrings = object({
+  required: ['de', 'en'],
+  additionalProperties: false,
+  properties: {
+    action: string(),
+    de: arrayOfStrings,
+    en: arrayOfStrings
+  }
+});
+
+export const widgets = {
+
+  text: contentPart('text', multiLangArrayOfStrings),
+
+  input: contentPart('input', object({
+    required: ['key', 'type', 'options'],
+    properties: {
+      key: string(),
+      type: { enum: ['slider', 'choices'] },
+      options: { type: 'object' }
+    }
+  })),
+
+  answer: contentPart('answer', object({
+    required: ['templates'],
+    additionalProperties: false,
+    properties: {
+      className: { enum: ['tip'] },
+      answerKey: array(),
+      answerContext: object({
+        patternProperties: { '.*': string() }
+      }),
+      templates: {
+        patternProperties: { '.*': multiLangArrayOfStrings }
+      }
+    }
+  })),
+
+  answerQuiz: contentPart('answerQuiz', object({
+    required: ['templates', 'key'],
+    additionalProperties: false,
+    properties: {
+      key: string(),
+      actions: array(),
+      className: { enum: ['tip'] },
+      answerKey: array(),
+      answerContext: object({
+        patternProperties: { '.*': string() }
+      }),
+      templates: {
+        patternProperties: { '.*': multiLangArrayOfStrings }
+      }
+    }
+  }))
+
 };
--- src/logic/questionnaires/validation.js
+++ src/logic/questionnaires/validation.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-shadow */
+
 export function validateQuestionnaire(questionnaire) {
   if (__DEV__) {
-    const ajv = require('ajv');
-    const schema = require('./schema').default;
-    const validate = ajv().compile(schema);
+    const ajv = require('ajv')();
+    const schema = require('./schema');
+    const validate = ajv.compile(schema.questionnaire);
     const valid = validate(questionnaire.cards);
     if (!valid) {
       const errors = JSON.stringify(validate.errors, null, 2);
       throw new Error(`Questionnaire is invalid - ${errors}`);
+    }
+    for (const card of questionnaire.cards) {
+      for (const widget of card.content) {
+        const type = Object.keys(widget)[0];
+        const validate = ajv.compile(schema.widgets[type]);
+        const valid = validate(widget);
+        if (!valid) {
+          const errors = JSON.stringify(validate.errors, null, 2);
+          throw new Error(`Widget "${ type }" is invalid - ${errors}`);
+        }
+      }
     }
   }
 }
