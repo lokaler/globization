887bc58da6a8dcb089bccdf811a0c650fff1c4d0
Sven König <sven@lokaler.de>
2016-04-12 12:51:47 +0200

    q: fixed and refactored Choices

--- src/components/Questionnaire/Card/widgets/Choices.js
+++ src/components/Questionnaire/Card/widgets/Choices.js
@@ -1,58 +1,53 @@
 import React, { PropTypes } from 'react';
-import cssModules from 'react-css-modules';
 import translate from 'logic/translate';
 import { logbuch } from 'logic/logbuch';
+import styles from './widgets.scss';
 
-@cssModules()
 export default class Choices extends React.Component {
 
   static propTypes = {
     id: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
     options: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired,
     questions: PropTypes.object.isRequired
   }
 
-  onChange = (evt) => {
-    const activeValue = evt.target.value;
-    this.props.actions.updateUserInput(this.props.id, activeValue);
-  }
-
-  onClick(activeValue) {
-    this.props.actions.updateUserInput(this.props.id, activeValue);
-    logbuch({ key: this.props.id, activeValue });
-  }
-
-  isActiveRadio(activeValue) {
-    const currUserInput = this.props.questions.inputValues[this.props.id];
-    return currUserInput &&
-      typeof currUserInput.value !== 'undefined' &&
-      currUserInput.value === activeValue;
-  }
-
-  createRadioButton(option, index) {
-    const { id } = this.props;
-    const isSelected = this.isActiveRadio(option[0]);
-    const key = `${ id }_${ index }`;
-    const value = option[0];
-    const label = translate(option[1]);
-    const boundOnClick = this.onClick.bind(this, value);
-
-    return (
-      <div key={ key } onClick={ boundOnClick } style={{ cursor: 'pointer' }}>
-        <input type="radio" value={ value } onChange={ this.onChange } checked={ isSelected }/>
-        <span styleName="choice-label">{ label }</span>
-      </div>
-    );
+  setValue(value) {
+    const { id, actions } = this.props;
+    actions.updateUserInput(id, value);
+    logbuch({ key: id, value });
   }
 
   render() {
-    const choices = [...this.props.options.choices];
-    const Radios = choices.map(this.createRadioButton.bind(this));
+    const { id, options, questions } = this.props;
+    const currentValue = questions.inputValues[id];
+
+    const Radios = options.choices.map((option) => {
+      const [value, label] = option;
+      const onClick = this.setValue.bind(this, value);
+      const noop = () => {}; // eslint-disable-line arrow-body-style
+
+      return (
+        <div
+          key={ `${ id }_${ value }` }
+          className={ styles.pointer }
+          onClick={ onClick }
+        >
+          <input
+            type="radio"
+            value={ value }
+            checked={ value === currentValue }
+            onChange={ noop } // so react doesn't complain
+          />
+          <span className={ styles.choicelabel }>
+            { translate(label) }
+          </span>
+        </div>
+      );
+    });
 
     return (
-      <div styleName="widget">
+      <div className={ styles.widget }>
         { Radios }
       </div>
     );
