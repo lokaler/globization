5b2e6b45003c917f20c240b2d0c24f40a390620e
Christopher Pietsch <cpietsch@gmail.com>
2016-04-13 16:24:07 +0200

    deploy script

---
+++ sbin/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ../
+NODE_ENV=production npm run build
+cp -r dist /tmp/
+git checkout testing
+cp -r /tmp/dist .
+git add -A
+git commit -a -m "deploy"
+git push
+git checkout 04-16
+git pull
+
+exit 0
