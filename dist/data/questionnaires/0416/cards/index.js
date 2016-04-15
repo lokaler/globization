3218f940ab48bf648be9dc7830e7412c671898ae
lorz <matzat@lokaler.dez>
2016-04-15 09:03:01 +0200

    deploy

---
+++ dist/data/questionnaires/0416/cards/index.js
@@ -0,0 +1,9 @@
+export default [
+  require('./00-intro.json'),
+  require('./01-bmi.json'),
+  require('./04-kids.json'),
+  require('./05-diabetes.json'),
+  require('./06-sports.json'),
+  require('./07-alcohol.json'),
+  require('./08-summary.json')
+];
