dc5906f9fa9cb78ce84cf189ec83ade8b9e52f62
lorz <matzat@lokaler.dez>
2016-04-15 08:54:32 +0200

    changes to nearly everything

--- dist/data/questionnaires/0416/cards/index.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require('./00-intro.json'),
-  require('./01-bmi.json'),
-  require('./04-kids.json'),
-  require('./05-diabetes.json'),
-  require('./06-sports.json'),
-  require('./07-alcohol.json'),
-  require('./08-summary.json')
-];
