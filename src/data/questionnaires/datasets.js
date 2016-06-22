a8ac3d222cd430084eac33f950043073f09f5630
Christopher Pietsch <cpietsch@gmail.com>
2016-06-22 13:52:44 +0200

    q: deleted dataset jsons

--- src/data/questionnaires/datasets.js
+++ src/data/questionnaires/datasets.js
@@ -6,8 +6,9 @@ import master from 'data/map/master.csv';
 
 
 export function prepareDataset(dataset) {
-  console.log(dataset);
+  // console.log(dataset);
   for (const d of dataset.data) {
+    d.value = +d.value;
     const e = _.find(master, { alpha3: d.iso });
     const t = _.find(topofeatures, (c) => c.properties.iso === d.iso);
     d.onMap = t !== undefined;
@@ -17,3 +18,12 @@ export function prepareDataset(dataset) {
   dataset.vergleichDomain = d3.extent(dataset.data, (d) => d.vergleich);
   /* eslint-enable */
 }
+
+
+export const emptyDataset = {
+  key: 'none',
+  data: [],
+  translate: [0, 90],
+  domain: [0, 1],
+  scale: 1
+};
