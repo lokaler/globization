a5c300cd96ac948242403c89f642777eda31ca14
Lorenz Matzat <matzat@lokaler.de>
2016-06-24 12:43:00 +0200

    datasets and cards for round 4

--- src/data/questionnaires/0616/cards/index.js
+++ src/data/questionnaires/0616/cards/index.js
@@ -1,3 +1,9 @@
 export default [
-  require('./00-intro.json')
+  require('./00-intro.json'),
+  require('./01-refugees.json'),
+  require('./02-refugees-internal.json'),
+  require('./03-violence.json'),
+  require('./04-threat.json'),
+  require('./05-slavery.json'),
+  require('./06-explore.json')
 ];
