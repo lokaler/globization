62ec1550a30d20498af8ba38df4c2988b28d72fe
Lorenz Matzat <matzat@lokaler.de>
2016-07-06 15:50:34 +0200

    added card journalists, small fixes

--- src/data/questionnaires/0616/cards/index.js
+++ src/data/questionnaires/0616/cards/index.js
@@ -5,5 +5,6 @@ export default [
   require('./03-violence.json'),
   require('./04-threat.json'),
   require('./05-slavery.json'),
-  require('./06-explore.json')
+  require('./06-journalists.json'),
+  require('./07-explore.json')
 ];
