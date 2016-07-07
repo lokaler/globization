d5cb9a1911c71b6aa6b9558f8b970513bb4a5dfd
Christopher Pietsch <cpietsch@gmail.com>
2016-07-07 11:41:59 +0200

    deploy

--- dist/data/questionnaires/datasets.js
+++ dist/data/questionnaires/datasets.js
@@ -6,9 +6,15 @@ import master from 'data/map/master.csv';
 
 
 export function prepareDataset(dataset) {
+  // console.log(dataset);
   for (const d of dataset.data) {
-    const e = _.find(master, { alpha3: d.iso });
+    d.value = +d.value;
+    let e = _.find(master, { alpha3: d.iso });
     const t = _.find(topofeatures, (c) => c.properties.iso === d.iso);
+    if (!e) {
+      console.warn(d.iso, 'not in masterdataset');
+      e = { vergleich: 0 };
+    }
     d.onMap = t !== undefined;
     d.vergleich = e.vergleich;
   }
@@ -16,3 +22,12 @@ export function prepareDataset(dataset) {
   dataset.vergleichDomain = d3.extent(dataset.data, (d) => d.vergleich);
   /* eslint-enable */
 }
+
+
+export const emptyDataset = {
+  key: 'none',
+  data: [],
+  translate: [0, 90],
+  domain: [0, 1],
+  scale: 1
+};
