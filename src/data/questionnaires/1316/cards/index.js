c0970f2ce0595f81e83954600f0630390759d93e
Christopher Pietsch <cpietsch@gmail.com>
2017-05-29 13:24:29 +0200

    removed old questionaires

--- src/data/questionnaires/1316/cards/index.js
+++
@@ -1,9 +0,0 @@
-export default [
-  require('./00-intro.json'),
-  require('./01-topten.json'),
-  require('./02-topone.json'),
-  require('./03-eightytwenty.json'),
-  require('./04-gini.json'),
-  require('./05-toponewealth.json'),
-  require('./06-explore.json')
-];
