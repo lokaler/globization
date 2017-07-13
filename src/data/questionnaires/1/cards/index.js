97d2c635ffdfabe492fe75001378c5b53a1aa439
Christopher Pietsch <cpietsch@gmail.com>
2017-07-13 16:03:09 +0200

    New test Question

--- src/data/questionnaires/1/cards/index.js
+++ src/data/questionnaires/1/cards/index.js
@@ -1,8 +1,8 @@
 export default [
   require('./00-intro.json'),
   require('./01-wasserverbrauch.json'),
-  require('./02-zugang.json')
-  // require('./03-childlabour.json'),
+  require('./02-zugang.json'),
+  require('./03-test.json')
   // require('./04-youthunemployment.json'),
   // require('./05-unemployment.json'),
   // require('./06-maternyleave.json'),
