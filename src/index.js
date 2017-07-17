e613af25da95d2041cffaf23a43682a7171f5892
Christopher Pietsch <cpietsch@gmail.com>
2017-07-17 17:41:55 +0200

    Added babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import 'babel-polyfill'
 import App from './App';
 import store from './store';
 import './styles/styles.css';
