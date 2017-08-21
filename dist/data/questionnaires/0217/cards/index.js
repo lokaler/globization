974f3442ce62896bea901548ea9cdea628fc52f2
Christopher Pietsch <cpietsch@gmail.com>
2017-08-21 10:28:46 +0200

    deploy

---
+++ dist/data/questionnaires/0217/cards/index.js
@@ -0,0 +1,9 @@
+export default [
+  require('./00-intro.json'),
+  require('./01-kalorien.json'),
+  require('./02-unterernaehrung.json'),
+  require('./03-lebensmittel.json'),
+  require('./04-verzoegerung.json'),
+  require('./05-wfp.json'),
+  require('./06-explore.json')
+];
